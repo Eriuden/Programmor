e56c8f78a54063ae0ee63ad6a9a8edc35dad5a16
Eriuden <antoinecarrette@hotmail.fr>
2025-11-13 16:13:40 +0100

    build: Client app.tsx 13/11/2025

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,55 @@
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Header from './Components/Header'
+import { useDispatch } from "react-redux" 
+import { getUser } from './redux/actions/User.action'
 import './App.css'
+import { UserProfile } from './pages/UserProfile'
+import { Searcher } from './Components/Searcher'
+import MessageIndex from './pages/MessageIndex'
+import { ResetPassword } from './pages/ResetPassword'
+import UpdatePassword from './pages/UpdatePassword'
+import { Home } from './pages/Home'
 
 function App() {
 
+  type appDispatch = () => any 
+
+  const [uid, setUid] = useState("")
+  const useAppDispatch = () => useDispatch<appDispatch>()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const fetchToken = async () => {
+    await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials: true
+    })
+    .then((res) => {
+      console.log(res);
+      setUid(res.data)
+    })
+    .catch(() => console.log("Pas de tokens"))
+    }
+    fetchToken()
+    if (uid) getUser(uid, dispatch)
+  }, [uid])
+
   return (
-    <>
-    </>
+    <div className="App">
+      <Header/>
+      <Searcher/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/user-profile/:id" element={<UserProfile/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path={"/update-password"} element={<UpdatePassword/>}/>
+        <Route path="/messages/:id" element={<MessageIndex/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
