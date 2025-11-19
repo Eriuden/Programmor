fe4bfc45f218c882ea62cc2b7d46c28ee083c4cd
Eriuden <antoinecarrette@hotmail.fr>
2025-11-19 14:30:21 +0100

    build: client user profile page 19/11/2025

--- Client/src/Pages/UserProfile.tsx
+++ Client/src/Pages/UserProfile.tsx
@@ -1,43 +1,53 @@
-import {useState, useEffect} from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getUsers } from "../redux/actions/Users.action" 
-import UserCard from "../Components/UserCard"
-import { isEmpty } from "../utils"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "../redux/actions/User.action";
+import UpdatePassword from "./UpdatePassword";
 
-export const Home = () => {
-    const [loadCard, setLoadCard] = useState(false)
-    const [count, setCount] = useState(0)
-    const dispatch = useDispatch()
-    const users = useSelector((state:any)=> state.allUsersReducer)
+export const UserProfile = (userProps:any) => {
+
+    const userData = useSelector((state: any) => state.userReducer)
+    const dispatch = useDispatch
+
+    const [name, setName] = useState(userData.name)
+    const [gender, setGender] = useState(userData.gender)
+    const [address, setAddress] = useState(userData.adress)
+    const [updateForm, setUpdateForm] = useState(false)
   
-  
-    const loadMore = () => {
-      if (window.innerHeight + document.documentElement.scrollTop + 1 >
-        document.scrollingElement!.scrollHeight)
-        {
-          setLoadCard(true)
-        }
+    const handleUpdate = () => {
+      updateUser(userProps, dispatch)
+      setUpdateForm(false)
     }
-  
-    useEffect(()=> {
-      if (loadCard) {
-        getUsers(count, dispatch)
-        setLoadCard(false)
-        setCount(count + 10)
-      }
-      window.addEventListener("scroll", loadMore)
-    }, [loadCard, dispatch, count])
-    
     return (
       <div>
+        <h1>Profil de {userData.name}</h1>
+        <h3>De genre {userData.gender}</h3>
+        <h3>{userData.address}</h3>
+  
+        {updateForm === false && (
+          <>
+            <p onClick={() => setUpdateForm(!updateForm)}>Modifier votre profil</p>
+          </>
+        )}
+  
+        {updateForm && (
+          <>
+            <input type="text" defaultValue={gender} onChange={(e)=> setGender
+            (e.target.value)}/>
+            <input type="text" defaultValue={name} onChange={(e)=> setName
+            (e.target.value)}/>
+            <textarea defaultValue={address} onChange={(e)=>
+            setAddress(e.target.value)}/>
+            
+  
+            <button onClick={handleUpdate}>Valider les modifications</button>
+          </>
+        )}
+  
         <div>
-          <ul>
-            {!isEmpty(users[0]) &&
-              users.map((article:any) => {
-                return <UserCard articleProps={article} key={article._id}/>
-            })}
-          </ul>
+          <h2>Modifier votre mot de passe</h2>
+          <UpdatePassword/>
         </div>
+        
       </div>
     )
 }
